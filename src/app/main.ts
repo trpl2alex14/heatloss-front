b7fa81a2d50580adcf6680d734b172949a59c259
trpl2alex14 <av@itentaro.ru>
2025-07-24 16:02:23 +0500

    Добавлена локализация для PrimeVue: DatePicker

--- src/app/main.ts
+++ src/app/main.ts
@@ -7,5 +7,16 @@ import { myStylePreset } from "@shared/utils/presets";
 
 createApp(App)
 	.use(router)
-	.use(PrimeVue, { theme: { preset: myStylePreset } })
+	.use(PrimeVue, { 
+		locale: {
+			clear: "Очистить",
+			today: "Сегодня",
+			monthNamesShort: ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"],
+			monthNames: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
+			dayNamesShort: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],			
+			dayNames: ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"],			
+			dayNamesMin: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],			
+		},
+		theme: { preset: myStylePreset } 
+	})
 	.mount("#app");
