ea1a614a33d95b1b2453d6985235f346b332f08c
trpl2alex14 <av@itentaro.ru>
2025-08-06 17:36:52 +0500

    fix: исправлена логика склонения слов в функции plural для корректного отображения чисел

--- src/shared/utils/text.ts
+++ src/shared/utils/text.ts
@@ -3,8 +3,8 @@
 //5 - ... товаров
 export function plural(value: number, words: string[], viewCount = false): string {
     let result = viewCount ? value + ' ' : '';
-    if (value % 10 === 1) result += words[0];
-    else if (value % 10  > 1 && value % 10  < 5) result += words[1];
+    if (value % 10 === 1 && value % 100 !== 11) result += words[0];
+    else if (value % 10 >= 2 && value % 10 <= 4 && (value % 100 < 10 || value % 100 >= 20)) result += words[1];
     else result += words[2];
 
     return result;
