50cad3b44a3f800dc11c1c0f7c62711fa4c80ad9
Alex <trpl2alex14@bk.ru>
2025-09-17 16:12:29 +0500

    feat: добавлены параметры времени жизни для сообщений об ошибках и предупреждениях в useMessage

--- src/shared/composables/useMessage.ts
+++ src/shared/composables/useMessage.ts
@@ -3,12 +3,12 @@ import { useToast } from "primevue/usetoast";
 export const useMessage = () => {
 	const toast = useToast();
 
-	const error = (message: string) => {
-		toast.add({ severity: "error", summary: "Ошибка", detail: message, life: 3000 });
+	const error = (message: string, life: number = 3000) => {
+		toast.add({ severity: "error", summary: "Ошибка", detail: message, life });
 	};
 
-	const warning = (message: string) => {
-		toast.add({ severity: "warn", summary: "Внимание", detail: message, life: 3000 });
+	const warning = (message: string, life: number = 3000) => {
+		toast.add({ severity: "warn", summary: "Внимание", detail: message, life });
 	};
 
 	return { error, warning };
