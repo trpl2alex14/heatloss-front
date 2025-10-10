7e27a7bbd483e1d32442ab0897cb181197086c76
Alex <trpl2alex14@bk.ru>
2025-10-10 13:47:28 +0500

    ref: import for Material.vue

--- src/features/directories/components/Layout.vue
+++ src/features/directories/components/Layout.vue
@@ -2,11 +2,11 @@
 	<SubMenuLayout>
 		<template #header>
 			<Head title="Справочники" subtitle="" />
-		</template>		
+		</template>
 	</SubMenuLayout>
 </template>
 
 <script setup lang="ts">
 import SubMenuLayout from "@layouts/SubMenuLayout.vue";
-import Head from "@shared/components/Head.vue";
+import Head from "../../../shared/components/Head.vue";
 </script>
